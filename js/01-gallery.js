db092eec28dcc710f40ebb836e00d7441add83f5
Evgenij <e.vasilevich35@gmail.com>
2022-02-15 06:32:14 +0300

    Изменение настроек

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const createGalleryItem = createImagesItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryItem);
+galleryContainer.addEventListener('click', onImgLinkClick);
+
+
+function createImagesItem(images) {
+  return images
+    .map(({ preview, original, description }) => {
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join('');
+}
+
+let originalImg;
+
+function onImgLinkClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  originalImg = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" >`,
+  );
+
+  originalImg.show()
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryListRef = document.querySelector('.gallery');
+
+const galleryItem = galleryItems.map(({ preview, original, description }) => {
+  return `<li>
+   <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt=${description}/>
+    </a>
+  </li>`;
+});
+
+galleryListRef.insertAdjacentHTML('beforeend', galleryItem.join(''));
+
+
+let gallery = new SimpleLightbox('.gallery a',{
+  captions: true,
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
+
+// const galleryItemMarkup = galleryItems.map(({ preview, original, description }) => {
+//   return `
+//   <li>
+//     <a class="gallery__item" href="${original}">
+//       <img class="gallery__image" src="${preview}" alt="${description}"/></a>
+//   </li>`;
+// });
+
+// const galleryContainerRef = document.querySelector('.gallery')
+// galleryContainerRef.insertAdjacentHTML('beforeend', galleryItemMarkup).join('')
+
+//  let gallery = new SimpleLightbox('.gallery a',{
+//   captions: true,
+//   captionsData: 'alt',
+//   captionsDelay: 250,
+// });
